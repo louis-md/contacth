a5dca78b5b3a531a148ccc364865acec98f7aaaf
Louis MD <louis@margot-duclot.fr>
2020-02-10 16:43:22 +0100

    Create and assign contact to user when signup

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb://localhost/contacth', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
 
 mongoose.connection.on("connected", () =>
-  console.log("yay mongodb connected :)")
+  console.log(`Connected to MongoDB at: ${process.env.MONGO_URI}`)
 );
 
 mongoose.connection.on("error", () =>
-  console.log("nay db connection error :(")
+  console.log("Error connecting to MongoDB")
 );
